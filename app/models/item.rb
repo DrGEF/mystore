3c97fd91e434f04dded2695b3d9e367f325e1196
drgef <thedrgef@gmail.com>
2015-04-02 00:09:08 +0300

    reorg 6

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,11 @@
 class Item < ActiveRecord::Base
 	validates :price, numericality: { greater_than: 0, allow_nil: true }
 	validates :name, :description, presence: true
+
+after_initialize { puts "initialized!" } # Item.new; Item.first
+after_save       { puts "saved" } # Item.save; Item.create; Item.upd_attr
+after_create     { puts "created" }
+after_update     { puts "updated" }
+after_destroy    { puts "destroyed!" } # Item.destroy
+
 end
