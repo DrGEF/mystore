953f67fa355fb577b296e2aa57519db91dea4995
drgef <thedrgef@gmail.com>
2015-04-06 22:33:28 +0300

    reorg 9

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,4 +5,28 @@ class ItemsController < ApplicationController
 		render text: @items.map { |i| "#{i.name}: #{i.price}"}.join("<br/>")
 	end
 
+	def show
+
+	end
+
+	def new
+		
+	end
+
+	def edit
+		
+	end
+
+	def create
+		create text: "item created!"
+	end
+
+	def destroy
+		
+	end
+
+	def update
+		
+	end
+
 end
